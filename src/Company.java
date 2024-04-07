f5f54ba43155c1c99d40dced2752d6caf9a4601a
jamez7 <kulikowskijacob@gmail.com>
2024-04-07 19:19:48 +0200

    2

--- src/Company.java
+++ src/Company.java
@@ -8,7 +8,7 @@
             Employees[4]= new Employee();
 
 
-            System.out.println("Dane pracownika o indeksie 3: " + Employees[1].toString());
+            System.out.println("Dane pracownika o indeksie 3: " + Employees[3].toString());
 
             Employees[3].setSalary(1000);
 
